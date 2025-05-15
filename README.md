bd15601db7a902503ccfcc7d39912520e25ccefd
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2025-05-15 21:54:43 +0200

    Update font data
    Use modified version of NewStroke which is KiCad font used to engrave enclosure's labels. Add missing glyphs to get full ASCII support. Generate glyph data from PBM image, remove old HTML editor.

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 KiCad files (board) can be previewed in the web browser: https://kicanvas.org<br/>OpenSCAD files (enclosure) can be previewed in the web browser: https://openscad.cloud/openscad/
 
 License terms for software and hardware are present in the [LICENSE.md](./LICENSE.md) file.
+
+## Updating font data
+
+Font data is stored in the `tools/font.pbm` image file, with `tools/font.txt` containing font widths. The image file can be edited in GIMP.
+
+These source files can be converted into working Go code, using the `tools/convert.py` script, which should work with any modern version of Python:
+
+```bash
+cd tools
+python convert.py > ../firmware/hal/display/font.go
+```
---
+++ tools/convert.py
@@ -0,0 +1,48 @@
+# get and process glyph data
+
+with open("font.pbm", "rt") as glyph_file:
+    lines = glyph_file.readlines()
+
+assert lines[0].strip() == "P1"
+if lines[1].startswith("#"):
+    lines.pop(1)
+
+t_width, t_height = lines[1].split(" ")
+t_width = int(t_width)
+t_height = int(t_height)
+
+data = [0] * t_width
+x = 0
+y = 0
+
+for line in lines[2:]:
+    for value in line.strip():
+        if value == "1":
+            data[x] |= (1 << y)
+        x += 1
+        if x == t_width:
+            x = 0
+            y += 1
+
+# get glyph widths
+
+with open("font.txt", "rt") as metadata_file:
+    g_widths = [int(w) for w in metadata_file.read().splitlines()]
+
+# build Go data
+
+print("package display")
+print()
+print("var font = [][]uint16{")
+
+d = 0
+g = 32
+for width in g_widths:
+    print(f"/*  {chr(g)}  */ {{", end="")
+    for n in data[d:d+width]:
+        print(f"0x{n:04X}, ", end="")
+    print(f"}},")
+    d += width
+    g += 1
+
+print("}")
