dff42e6291e7610f081ec527eb18ce2caf8c4f8c
Mateusz Nowak <118080320+mateusznowakdev@users.noreply.github.com>
2025-06-18 20:09:19 +0200

    Use littlefs for storage

--- firmware/apps/notes/note.go
+++ firmware/apps/notes/note.go
@@ -1,27 +1,41 @@
 package notes
 
 import (
+	"errors"
+	"machine"
+	"os"
 	"slices"
 	"time"
 
-	"eclair/apps"
-	"eclair/storage"
+	"tinygo.org/x/tinyfs/littlefs"
 )
 
+const name = "note.txt"
+const maxSize = 256
 const writeDelay = 1500 // ms
 
 type Note struct {
-	file *storage.File
+	fs *littlefs.LFS
 
+	data      []byte
 	cursor    int
 	timestamp int64
 }
 
-func NewNote() Note {
-	bounds := apps.Bounds["notes"]
-	file := storage.NewFile(bounds)
+func NewNote() (*Note, error) {
+	fs := littlefs.New(machine.Flash)
+	fs.Configure(&littlefs.Config{
+		CacheSize:     256,
+		LookaheadSize: 64,
+		BlockCycles:   512,
+	})
 
-	return Note{file: file}
+	err := fs.Mount()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Note{fs: fs}, nil
 }
 
 func (n *Note) cursorLeft() {
@@ -31,14 +45,14 @@ func (n *Note) cursorLeft() {
 }
 
 func (n *Note) cursorRight() {
-	if n.cursor < len(n.file.Data) {
+	if n.cursor < len(n.data) {
 		n.cursor += 1
 	}
 }
 
 func (n *Note) delete() {
 	if n.cursor > 0 {
-		n.file.Data = slices.Delete(n.file.Data, n.cursor-1, n.cursor)
+		n.data = slices.Delete(n.data, n.cursor-1, n.cursor)
 		n.cursor -= 1
 	}
 
@@ -50,8 +64,8 @@ func (n *Note) dirty() bool {
 }
 
 func (n *Note) insert(b byte) {
-	if len(n.file.Data) < n.file.MaxSize() {
-		n.file.Data = slices.Insert(n.file.Data, n.cursor, b)
+	if len(n.data) < maxSize {
+		n.data = slices.Insert(n.data, n.cursor, b)
 		n.cursor += 1
 	}
 
@@ -60,7 +74,7 @@ func (n *Note) insert(b byte) {
 
 func (n *Note) last() byte {
 	if n.cursor > 0 {
-		return n.file.Data[n.cursor-1]
+		return n.data[n.cursor-1]
 	} else {
 		return 0
 	}
@@ -70,24 +84,61 @@ func (n *Note) markDirty() {
 	n.timestamp = time.Now().UnixMilli()
 }
 
-func (n *Note) read() (bool, error) {
-	success, err := n.file.Read()
-	n.cursor = len(n.file.Data)
+func (n *Note) read() error {
+	f, err := n.fs.OpenFile(name, os.O_CREATE|os.O_RDONLY)
+	if err != nil {
+		return err
+	}
 
-	return success, err
+	inf, _ := n.fs.Stat(name) // f.Stat().Size() is crashing
+	size := inf.Size()
+	if size > maxSize {
+		return errors.New("file too large")
+	}
+
+	n.data = make([]byte, size, maxSize)
+	n.cursor = int(size)
+
+	if size > 0 {
+		_, err = f.Read(n.data)
+		if err != nil {
+			return err
+		}
+	}
+
+	_ = f.Close()
+	return nil
 }
 
 func (n *Note) replace(b byte) {
 	if n.cursor > 0 {
-		n.file.Data[n.cursor-1] = b
+		n.data[n.cursor-1] = b
 	}
 
 	n.markDirty()
 }
 
-func (n *Note) write() (bool, error) {
+func (n *Note) write() error {
 	n.timestamp = 0
-	return true, n.file.Write()
+
+	f, err := n.fs.OpenFile("note.tmp", os.O_CREATE|os.O_WRONLY|os.O_TRUNC)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(n.data)
+	if err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+
+	_ = n.fs.Rename("note.tmp", name) // atomic on littlefs
+
+	return nil
 }
 
 func (n *Note) writeDelayed() (bool, error) {
@@ -95,5 +146,6 @@ func (n *Note) writeDelayed() (bool, error) {
 		return false, nil
 	}
 
-	return n.write()
+	err := n.write()
+	return err == nil, err
 }
--- firmware/go.mod
+++ firmware/go.mod
@@ -2,6 +2,9 @@ module eclair
 
 go 1.23
 
-require tinygo.org/x/drivers v0.32.0
+require (
+	tinygo.org/x/drivers v0.32.0
+	tinygo.org/x/tinyfs v0.5.0
+)
 
 require github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
